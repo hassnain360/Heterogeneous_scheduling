ea5159ff409b78bc1f5f59b9ba74b580079046e4
hassnain360 <hasnain@bu.edu>
2021-02-10 00:27:46 -0500

    Updated higgs-sklearn.py

--- higgs-sklearn.py
+++ higgs-sklearn.py
@@ -6,189 +6,191 @@ import numpy
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 
-X_train = pd.read_csv("HIGGS.csv", nrows = 10000000, header = None)
+X_train = pd.read_csv("HIGGS.csv", nrows = 10000000, header = None) # using 10M as training set
 y_train = np.int8(X_train[0])
 X_train = np.asarray(X_train.drop([0], axis = 1))
 
-X_test = pd.read_csv("HIGGS.csv", skiprows =  10000000, header = None)
+X_test = pd.read_csv("HIGGS.csv", skiprows =  10000000, header = None) # using 1M as testing set
 y_test = np.int8(X_test[0])
 X_test = np.asarray(X_test.drop([0], axis=1))
 
 ########################################################################################################
-print('Training Random Forest with HIGGS - 1 Forest ...')
-RF_HIGGS_1 = RandomForestClassifier(n_estimators=1)
-RF_HIGGS_1.fit(X_train, y_train)
+#print('Training Random Forest with HIGGS - 1 Forest ...')
+#RF_HIGGS = RandomForestClassifier(n_estimators=1)
+#RF_HIGGS.fit(X_train, y_train)
+#pickle.dump(RF_HIGGS,open(filename,'wb+'))
 filename='./models/RF_HIGGS_SK_1.sav'
-pickle.dump(RF_HIGGS_1,open(filename,'wb+'))
+RF_HIGGS = pickle.load(open(filename, 'rb'))
 print('Scoring Random Forest with HIGGS - 1 Forest ...')
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1])
+RF_HIGGS.predict(X_test[:1])
 end = (time.time() - start_time)*1000
 print(end)
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:100])
-end = (time.time() - start_time)*1000
-print(end)
-
-
-start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1000])
+RF_HIGGS.predict(X_test[:100])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:10000])
+RF_HIGGS.predict(X_test[:1000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:100000])
+RF_HIGGS.predict(X_test[:10000])
+end = (time.time() - start_time)*1000
+print(end)
+
+
+start_time = time.time()
+RF_HIGGS.predict(X_test[:100000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1000000])
+RF_HIGGS.predict(X_test[:1000000])
 end = (time.time() - start_time)*1000
 print(end)
 
 ########################################################################################################
-print('Training Random Forest with HIGGS - 32 Forest ...')
-RF_HIGGS_1 = RandomForestClassifier(n_estimators=32)
-RF_HIGGS_1.fit(X_train, y_train)
+#print('Training Random Forest with HIGGS - 32 Forest ...')
+#RF_HIGGS = RandomForestClassifier(n_estimators=32)
+#RF_HIGGS.fit(X_train, y_train)
+#pickle.dump(RF_HIGGS,open(filename,'wb+'))
 filename='./models/RF_HIGGS_SK_32.sav'
-pickle.dump(RF_HIGGS_1,open(filename,'wb+'))
-
+RF_HIGGS = pickle.load(open(filename, 'rb'))
+print('Scoring Random Forest with HIGGS - 32 Forest ...')
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1])
+RF_HIGGS.predict(X_test[:1])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:100])
+RF_HIGGS.predict(X_test[:100])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1000])
+RF_HIGGS.predict(X_test[:1000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:10000])
+RF_HIGGS.predict(X_test[:10000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:100000])
+RF_HIGGS.predict(X_test[:100000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1000000])
+RF_HIGGS.predict(X_test[:1000000])
 end = (time.time() - start_time)*1000
 print(end)
 
 ########################################################################################################
-print('Training Random Forest with HIGGS - 64 Forest ...')
-RF_HIGGS_1 = RandomForestClassifier(n_estimators=64)
-RF_HIGGS_1.fit(X_train, y_train)
+#print('Training Random Forest with HIGGS - 64 Forest ...')
+#RF_HIGGS = RandomForestClassifier(n_estimators=64)
+#RF_HIGGS.fit(X_train, y_train)
+#pickle.dump(RF_HIGGS,open(filename,'wb+'))
 filename='./models/RF_HIGGS_SK_64.sav'
-pickle.dump(RF_HIGGS_1,open(filename,'wb+'))
-
-print('Scoring Random Forest with HIGGS - 1 Forest ...')
+RF_HIGGS = pickle.load(open(filename, 'rb'))
+print('Scoring Random Forest with HIGGS - 64 Forest ...')
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1])
+RF_HIGGS.predict(X_test[:1])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:100])
+RF_HIGGS.predict(X_test[:100])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1000])
+RF_HIGGS.predict(X_test[:1000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:10000])
+RF_HIGGS.predict(X_test[:10000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:100000])
+RF_HIGGS.predict(X_test[:100000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1000000])
+RF_HIGGS.predict(X_test[:1000000])
 end = (time.time() - start_time)*1000
 print(end)
 
 ########################################################################################################
-print('Training Random Forest with HIGGS - 1 Forest ...')
-RF_HIGGS_1 = RandomForestClassifier(n_estimators=128)
-RF_HIGGS_1.fit(X_train, y_train)
+#print('Training Random Forest with HIGGS - 128 Forests ...')
+#RF_HIGGS = RandomForestClassifier(n_estimators=128)
+#RF_HIGGS.fit(X_train, y_train)
+#pickle.dump(RF_HIGGS,open(filename,'wb+'))
 filename='./models/RF_HIGGS_SK_128.sav'
-pickle.dump(RF_HIGGS_1,open(filename,'wb+'))
-
-print('Scoring Random Forest with HIGGS - 1 Forest ...')
+RF_HIGGS = pickle.load(open(filename, 'rb'))
+print('Scoring Random Forest with HIGGS - 128 Forest ...')
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1])
+RF_HIGGS.predict(X_test[:1])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:100])
+RF_HIGGS.predict(X_test[:100])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1000])
+RF_HIGGS.predict(X_test[:1000])
 end = (time.time() - start_time)*1000
 print(end)
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:10000])
+RF_HIGGS.predict(X_test[:10000])
 end = (time.time() - start_time)*1000
 print(end)
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:100000])
+RF_HIGGS.predict(X_test[:100000])
 end = (time.time() - start_time)*1000
 print(end)
 
 
 start_time = time.time()
-RF_HIGGS_1.predict(X_test[:1000000])
+RF_HIGGS.predict(X_test[:1000000])
 end = (time.time() - start_time)*1000
 print(end)
 
